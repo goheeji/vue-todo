4a98e2a8f36c0d7175fd254c1ddbc383600ebf79
goheeji <gmlwl0489@daum.net>
2022-01-20 16:34:00 +0900

    Vuex - 스토어 속성 모듈화 방법

---
+++ vue-todo/src/store/mutations.js
@@ -0,0 +1,29 @@
+const addOneItem = (state, todoItem) => {
+    const obj = {completed:false, item: todoItem};
+    localStorage.setItem(todoItem, JSON.stringify(obj));
+    state.todoItems.push(obj);
+    };
+
+const removeOneItem = (state, payload) => {
+    localStorage.removeItem(payload.todoItem.item);
+    state.todoItems.splice(payload.index, 1);
+    };
+
+const toggleOneItem = (state, payload) => {
+    state.todoItems[payload.index].completed = !state.todoItems[payload.index].completed
+    //  localStorage에 데이터를 갱신
+    localStorage.removeItem(payload.todoItem.item);
+    localStorage.setItem(payload.todoItem.item, JSON.stringify(payload.todoItem));
+    };
+
+const clearAllItems = (state) => {
+    localStorage.clear();
+    state.todoItems = [];
+    };  
+
+    export{
+        addOneItem,
+        removeOneItem,
+        toggleOneItem,
+        clearAllItems
+    }
